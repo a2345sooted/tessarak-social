aac11780809e2e0d0bd8ca16e287b9a968d550c3
a2345sooted <rcschupp@gmail.com>
2023-03-25 02:12:00 -0500

    wrapping up for a bit, got android building a release apk

---
+++ build-android-release-apk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd android
+./gradlew clean
+cd ..
+rm -rf android/app/src/main/res/drawable-*
+npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle
+cd android
+./gradlew assembleRelease
+
+
