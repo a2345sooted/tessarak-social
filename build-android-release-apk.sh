331aea16b74b5df7046d06450d7da194232f699c
a2345sooted <rcschupp@gmail.com>
2023-03-27 01:27:14 -0500

    updated bad icon files for android

--- build-android-release-apk.sh
+++ build-android-release-apk.sh
@@ -2,6 +2,7 @@
 
 cd android
 ./gradlew clean
+./gradlew -stop
 cd ..
 rm -rf android/app/src/main/res/drawable-*
 npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle
