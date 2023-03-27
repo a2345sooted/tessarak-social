51e7ea4d27da4a3b48898a47773f47aadddc2224
a2345sooted <rcschupp@gmail.com>
2023-03-27 17:39:28 -0500

    working on stuff

--- src/stack-account/AccountSettingsScreen.tsx
+++ src/stack-account/AccountSettingsScreen.tsx
@@ -1,12 +1,32 @@
 import React, {useContext} from 'react';
-import {Divider, IconButton, Text} from 'react-native-paper';
+import {Button, Divider, IconButton, Text} from 'react-native-paper';
 import {AppContext} from '@app-ctx';
 import {Alert, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const AccountSettingsScreen = () => {
   const {colors} = useContext(AppContext);
   const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
+
+  async function handleDelete() {}
+
+  function confirmDelete() {
+    Alert.alert(
+      "You're 100 on this???",
+      'You for sure want to delete your Tessarak account?',
+      [
+        {text: 'Yes, delete it', onPress: handleDelete},
+        {
+          text: 'No, cancel',
+          onPress: () => {},
+        },
+      ],
+    );
+  }
+
+  async function handleSignout() {}
 
   return (
     <View style={{backgroundColor: colors.bg1, flex: 1}}>
@@ -28,12 +48,23 @@ const AccountSettingsScreen = () => {
         </Text>
       </View>
       <Divider />
-      <View style={{marginTop: 20, paddingHorizontal: 20}}>
-        <Text
-          variant="bodyLarge"
-          style={{fontWeight: 'bold', color: colors.text}}>
-          account settings
-        </Text>
+      <View style={{flex: 1}} />
+      <View style={{paddingBottom: 20, paddingHorizontal: 30}}>
+        <Button
+          uppercase
+          mode="outlined"
+          labelStyle={{color: colors.text, fontWeight: 'bold'}}
+          onPress={handleSignout}>
+          Sign Out
+        </Button>
+      </View>
+      <View style={{paddingBottom: insets.bottom + 20, paddingHorizontal: 30}}>
+        <Button
+          mode="text"
+          labelStyle={{color: colors.text}}
+          onPress={confirmDelete}>
+          Delete Account
+        </Button>
       </View>
     </View>
   );
