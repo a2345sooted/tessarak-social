43f7dfd628a5986d79715e1dbe285f4018f247d3
a2345sooted <rcschupp@gmail.com>
2023-04-09 18:47:02 -0500

    working

--- src/stacks/content-feed/TkVideoView.tsx
+++ src/stacks/content-feed/TkVideoView.tsx
@@ -1,12 +1,13 @@
-import React, { createRef, useContext } from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import { AppContext } from '@app-ctx';
-import { TkVideo } from '../../services/content';
+import React, {createRef, useContext, useState} from 'react';
+import {Alert, Dimensions, StyleSheet} from 'react-native';
+import {AppContext} from '@app-ctx';
+import {TkVideo} from '../../services/content';
 import BottomActionBar from './BottomActionBar';
 import VideoResource from 'react-native-video';
-import { DimensionFeedContext } from './DimensionView';
+import {DimensionFeedContext} from './DimensionView';
 import VideoPlayer from 'react-native-media-console';
 import BottomCaptionBar from './BottomCaptionBar';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 
 const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
@@ -19,27 +20,35 @@ export function TkVideoView({content}: TkVideoViewProps): JSX.Element {
   const videoPlayerRef = createRef<VideoResource>();
   const {selectedContentId} = useContext(DimensionFeedContext);
 
+  const [manuallyPaused, setManuallyPaused] = useState(false);
+
   return (
-    <View style={[styles.container, {backgroundColor: colors.bg1}]}>
-      <BottomActionBar />
-      <BottomCaptionBar />
-      <VideoPlayer
-        onEnd={() => videoPlayerRef.current?.seek(0)}
-        videoRef={videoPlayerRef}
-        controlTimeoutDelay={3000}
-        disableFullscreen
-        disableVolume
-        disableBack
-        disableSeekButtons
-        resizeMode="cover"
-        repeat={false}
-        paused={!(selectedContentId === content.id)}
-        style={{height: screenHeight - 95, zIndex: 1000}}
-        source={{
-          uri: content.videoUrl,
-        }}
-      />
-    </View>
+    <>
+      <TouchableWithoutFeedback
+        style={[styles.container, {backgroundColor: colors.bg1}]}
+        onLongPress={() => Alert.alert('long press')}
+        onPress={() => setManuallyPaused(!manuallyPaused)}>
+        <BottomActionBar />
+        <BottomCaptionBar />
+        <VideoPlayer
+          onEnd={() => videoPlayerRef.current?.seek(0)}
+          videoRef={videoPlayerRef}
+          controlTimeoutDelay={3000}
+          preventsDisplaySleepDuringVideoPlayback
+          disableFullscreen
+          disableVolume
+          disableBack
+          disableSeekButtons
+          resizeMode="cover"
+          repeat={false}
+          paused={!(selectedContentId === content.id) || manuallyPaused}
+          style={{height: screenHeight - 95, zIndex: 1000}}
+          source={{
+            uri: content.videoUrl,
+          }}
+        />
+      </TouchableWithoutFeedback>
+    </>
   );
 }
 
