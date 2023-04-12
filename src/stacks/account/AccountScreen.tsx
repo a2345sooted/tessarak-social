977646dd69ff923a6914404251a3781f6f0f1679
a2345sooted <rcschupp@gmail.com>
2023-04-12 13:40:49 -0500

    working

--- src/stacks/account/AccountScreen.tsx
+++ src/stacks/account/AccountScreen.tsx
@@ -8,7 +8,7 @@ import {maskPhoneNumber} from '@utils';
 import {triggerImpactLight} from '@haptic';
 import {PulseIndicator} from 'react-native-indicators';
 import Animated, {FadeInUp, FadeOutDown} from 'react-native-reanimated';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import {Avatar} from 'react-native-paper';
 
 const AccountScreen = () => {
@@ -40,8 +40,8 @@ const AccountScreen = () => {
           Command Center
         </Text>
         <IconButton
-          icon="cog"
-          iconColor={'#c66ef1'}
+          icon="rocket"
+          iconColor={colors.tessarak}
           size={22}
           onPress={() => {
             //@ts-ignore
@@ -50,8 +50,8 @@ const AccountScreen = () => {
         />
         <IconButton
           style={{marginLeft: -12}}
-          icon="rocket"
-          iconColor={colors.tessarak}
+          icon="cog"
+          iconColor={'#c66ef1'}
           size={25}
           onPress={() => {
             //@ts-ignore
@@ -60,68 +60,29 @@ const AccountScreen = () => {
         />
       </View>
       <Divider />
-      <View
-        style={{
-          marginTop: 20,
-          paddingHorizontal: 12,
-          flexDirection: 'row',
-          justifyContent: 'center',
-        }}>
-        <Avatar.Icon size={60} icon="space-invaders" />
-      </View>
-      <View
-        style={{
-          paddingHorizontal: 12,
-          marginTop: 12,
-        }}>
-        <Text
-          variant="titleLarge"
+      <ScrollView>
+        <View
           style={{
-            fontWeight: 'bold',
-            color: colors.text,
-            textAlign: 'center',
+            marginTop: 20,
+            paddingHorizontal: 12,
+            flexDirection: 'row',
+            justifyContent: 'center',
           }}>
-          @barehugger
-        </Text>
-        <Text
-          variant="labelSmall"
-          style={{
-            fontWeight: 'bold',
-            color: colors.text,
-            textAlign: 'center',
-          }}>
-          @tessarak.org
-        </Text>
-        <View style={{paddingHorizontal: 30, marginTop: 16}}>
-          <Text
-            variant="labelSmall"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-            }}>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo
-          </Text>
+          <Avatar.Icon size={60} icon="space-invaders" />
         </View>
-      </View>
-      <View
-        style={{
-          paddingHorizontal: 12,
-          flexDirection: 'row',
-          justifyContent: 'center',
-          marginTop: 20,
-        }}>
-        <TouchableOpacity style={{marginHorizontal: 8}}>
+        <View
+          style={{
+            paddingHorizontal: 12,
+            marginTop: 12,
+          }}>
           <Text
-            variant="displaySmall"
+            variant="titleLarge"
             style={{
               fontWeight: 'bold',
               color: colors.text,
               textAlign: 'center',
             }}>
-            644
+            @barehugger
           </Text>
           <Text
             variant="labelSmall"
@@ -130,125 +91,166 @@ const AccountScreen = () => {
               color: colors.text,
               textAlign: 'center',
             }}>
-            Followers
+            @tessarak.org
           </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{marginHorizontal: 8}}>
-          <Text
-            variant="displaySmall"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-              textAlign: 'center',
-            }}>
-            192
-          </Text>
-          <Text
-            variant="labelSmall"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-              textAlign: 'center',
-            }}>
-            Following
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View
-        style={{
-          paddingHorizontal: 12,
-          flexDirection: 'row',
-          justifyContent: 'center',
-          marginTop: 20,
-        }}>
-        <TouchableOpacity style={{marginHorizontal: 8}}>
-          <Text
-            variant="headlineMedium"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-              textAlign: 'center',
-            }}>
-            644
-          </Text>
-          <Text
-            variant="labelSmall"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-              textAlign: 'center',
-            }}>
-            Likes
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{marginHorizontal: 8}}>
-          <Text
-            variant="headlineMedium"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-              textAlign: 'center',
-            }}>
-            192
-          </Text>
-          <Text
-            variant="labelSmall"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-              textAlign: 'center',
-            }}>
-            Shares
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{marginHorizontal: 8}}>
-          <Text
-            variant="headlineMedium"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-              textAlign: 'center',
-            }}>
-            991
-          </Text>
-          <Text
-            variant="labelSmall"
-            style={{
-              fontWeight: 'bold',
-              color: colors.text,
-              textAlign: 'center',
-            }}>
-            Downloads
-          </Text>
-        </TouchableOpacity>
-      </View>
+          <View style={{paddingHorizontal: 30, marginTop: 16}}>
+            <Text
+              variant="labelSmall"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+              }}>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            paddingHorizontal: 12,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: 20,
+          }}>
+          <TouchableOpacity style={{marginHorizontal: 8}}>
+            <Text
+              variant="displaySmall"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              644
+            </Text>
+            <Text
+              variant="labelSmall"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              Followers
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginHorizontal: 8}}>
+            <Text
+              variant="displaySmall"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              192
+            </Text>
+            <Text
+              variant="labelSmall"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              Following
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            paddingHorizontal: 12,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: 20,
+          }}>
+          <TouchableOpacity style={{marginHorizontal: 8}}>
+            <Text
+              variant="headlineMedium"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              644
+            </Text>
+            <Text
+              variant="labelSmall"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              Likes
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginHorizontal: 8}}>
+            <Text
+              variant="headlineMedium"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              192
+            </Text>
+            <Text
+              variant="labelSmall"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              Shares
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginHorizontal: 8}}>
+            <Text
+              variant="headlineMedium"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              991
+            </Text>
+            <Text
+              variant="labelSmall"
+              style={{
+                fontWeight: 'bold',
+                color: colors.text,
+                textAlign: 'center',
+              }}>
+              Downloads
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-      <View style={{marginTop: 12, paddingHorizontal: 30}}>
-        <List.Item
-          titleStyle={{color: colors.text, fontWeight: 'bold'}}
-          title="Public"
-          right={() => <List.Icon color={colors.text} icon="arrow-right" />}
-        />
-        <Divider />
-        <List.Item
-          titleStyle={{color: colors.text, fontWeight: 'bold'}}
-          title="Private"
-          right={() => <List.Icon color={colors.text} icon="arrow-right" />}
-        />
-        <Divider />
-        <List.Item
-          titleStyle={{color: colors.text, fontWeight: 'bold'}}
-          title="Bookmarked"
-          right={() => <List.Icon color={colors.text} icon="arrow-right" />}
-        />
-        <Divider />
-        <List.Item
-          titleStyle={{color: colors.text, fontWeight: 'bold'}}
-          title="Liked"
-          right={() => <List.Icon color={colors.text} icon="arrow-right" />}
-        />
-        <Divider />
-      </View>
+        <View style={{marginTop: 12, paddingHorizontal: 30}}>
+          <List.Item
+            titleStyle={{color: colors.text, fontWeight: 'bold'}}
+            title="Public"
+            right={() => <List.Icon color={colors.text} icon="arrow-right" />}
+          />
+          <Divider />
+          <List.Item
+            titleStyle={{color: colors.text, fontWeight: 'bold'}}
+            title="Private"
+            right={() => <List.Icon color={colors.text} icon="arrow-right" />}
+          />
+          <Divider />
+          <List.Item
+            titleStyle={{color: colors.text, fontWeight: 'bold'}}
+            title="Bookmarked"
+            right={() => <List.Icon color={colors.text} icon="arrow-right" />}
+          />
+          <Divider />
+          <List.Item
+            titleStyle={{color: colors.text, fontWeight: 'bold'}}
+            title="Liked"
+            right={() => <List.Icon color={colors.text} icon="arrow-right" />}
+          />
+          <Divider />
+        </View>
+      </ScrollView>
     </SafeScreen>
   );
 };
